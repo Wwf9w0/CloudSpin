96a84b8dea733b7e2eb3f6159cdd6a29ce5ff386
wwf9w0 <aliemrekaradag1@gmail.com>
2024-11-09 02:06:58 +0800

    initial commit

---
+++ src/main/java/com/cloud/spin/service/CloudSpinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.spin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSpinServiceApplication.class, args);
+	}
+
+}
